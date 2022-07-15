36b9b1b5a5960ee7a232e5becebd600438152e22
Richard Hughes <richard@hughsie.com>
2022-07-15 12:41:16 +0100

    Export UefiScannerError
    It looks like this was missed from the export list.

--- fwhunt_scan/__init__.py
+++ fwhunt_scan/__init__.py
@@ -9,13 +9,14 @@ __email__ = "fwhunt@binarly.io"
 __version__ = "2.0.0"
 
 from .uefi_analyzer import UefiAnalyzer, UefiAnalyzerError
-from .uefi_scanner import UefiRule, UefiScanner
+from .uefi_scanner import UefiRule, UefiScanner, UefiScannerError
 from .uefi_te import TerseExecutableParser
 
 __all__ = [
     "UefiAnalyzer",
     "UefiRule",
     "UefiScanner",
+    "UefiScannerError",
     "TerseExecutableParser",
     "UefiAnalyzerError",
 ]
