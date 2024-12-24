03731ee3627f9a7be039a66a39cb6b1fda8c2bb4
yeggor <vasilenko.yegor@gmail.com>
2024-12-24 20:02:10 +0000

    add pyproject.toml, fix logo width (#65)

--- fwhunt_scan/__init__.py
+++ fwhunt_scan/__init__.py
@@ -4,10 +4,6 @@
 Tools for analyzing UEFI firmware and checking UEFI modules with FwHunt rules
 """
 
-__author__ = "FwHunt team"
-__email__ = "fwhunt@binarly.io"
-__version__ = "2.3.5"
-
 from .uefi_analyzer import UefiAnalyzer, UefiAnalyzerError
 from .uefi_extractor import UefiBinary, UefiExtractor
 from .uefi_scanner import UefiRule, UefiScanner, UefiScannerError
